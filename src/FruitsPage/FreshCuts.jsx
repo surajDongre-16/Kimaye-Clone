a83628012e2cfcd96c19871e232c251b0fe1792f
AshwiniPerewar@gmail.com <ashwiniperewar411@gmail.com>
2022-06-15 06:07:08 +0530

    cart css modified

--- src/FruitsPage/FreshCuts.jsx
+++ src/FruitsPage/FreshCuts.jsx
@@ -1,22 +1,28 @@
-import React, { useEffect} from 'react'
+import React, { useEffect,useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { addtoCart, fetchData, getSingleProduct } from '../store/product.action';
-import { Spinner,Alert,AlertIcon } from '@chakra-ui/react'
+import { Spinner,Button } from '@chakra-ui/react'
 import FruitNavbar from "./FruitNavbar"
 import  styles from "./Fruits.module.css"
 const FreshCuts = () => {
     const dispatch=useDispatch();
+    const[toggle,setToggle]=useState(false);
     const {getFruits}=useSelector((state)=>state.product)
     useEffect(()=>
     {
         dispatch(fetchData("fresh-cuts"));
     },[])
 
-    const add=(id)=>
+    const change=(id)=>
     {
-      dispatch(addtoCart("fresh-cuts",id));
+      setToggle(!toggle)
+    if(toggle)
+
+      dispatch(addtoCart("all-fruits",id));
     }
+
+
 
     const showProduct=(id)=>
     {
@@ -34,7 +40,8 @@ const FreshCuts = () => {
         (
             <div key={fruit.id}>
                 <Link to="/products"> <img src={fruit.image} onClick={()=>{showProduct(fruit.id)}} className={styles.fruitimage}/></Link>
-                <div className={styles.addtocart}  onClick={()=>{add(fruit.id)}}>CHOOSE YOUR PACK</div>
+                <Button  width="320px" colorScheme="green" marginLeft="-27px" marginTop="-30px" className={styles.addtocart}  onClick={()=>{
+                change(fruit.id)}}>{toggle ?  "ADD TO CART" :"CHOOSE YOUR PACK" }</Button>
             <p>{fruit.name}</p>
             <h3>₹{fruit.price}</h3>
             </div>
--- src/FruitsPage/FruitCombos.jsx
+++ src/FruitsPage/FruitCombos.jsx
@@ -1,22 +1,28 @@
-import React, { useEffect} from 'react'
+import React, { useEffect,useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { addtoCart, fetchData, getSingleProduct } from '../store/product.action';
 import  styles from "./Fruits.module.css"
 import FruitNavbar from "./FruitNavbar"
-import { Spinner,Alert,AlertIcon } from '@chakra-ui/react'
+import { Spinner,Button } from '@chakra-ui/react'
  const FruitCombos = () => {
     const dispatch=useDispatch();
+    const[toggle,setToggle]=useState(false);
     const {getFruits}=useSelector((state)=>state.product)
     useEffect(()=>
     {
         dispatch(fetchData("fruit-combos"));
     },[])
 
-    const add=(id)=>
+    const change=(id)=>
     {
-      dispatch(addtoCart("fruit-combos",id));
+      setToggle(!toggle)
+    if(toggle)
+
+      dispatch(addtoCart("all-fruits",id));
     }
+
+
 
 
     const showProduct=(id)=>
@@ -34,7 +40,8 @@ import { Spinner,Alert,AlertIcon } from '@chakra-ui/react'
         (
             <div key={fruit.id}>
                  <Link to="/products"> <img src={fruit.image} onClick={()=>{showProduct(fruit.id)}} className={styles.fruitimage}/></Link>
-                <div className={styles.addtocart}  onClick={()=>{add(fruit.id)}}>CHOOSE YOUR PACK</div>
+                 <Button  width="320px" colorScheme="green" marginLeft="-27px" marginTop="-30px" className={styles.addtocart}  onClick={()=>{
+                change(fruit.id)}}>{toggle ?  "ADD TO CART" :"CHOOSE YOUR PACK" }</Button>
             <p>{fruit.name}</p>
             <h3>₹{fruit.price}</h3>
             </div>
